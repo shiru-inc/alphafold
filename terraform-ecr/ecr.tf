54ea7f2ee3d35ba1c6b14ccbce1606f8914132a6
Lawrence Lee <3534824+lawrlee@users.noreply.github.com>
2022-06-17 11:29:07 -0700

    feat: add ci infrastructure for building docker image

---
+++ terraform-ecr/ecr.tf
@@ -0,0 +1,40 @@
+resource "aws_ecr_repository" "etl-alphafold" {
+  name = "alphafold"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+data "aws_iam_policy_document" "data-account-policy" {
+  statement {
+    sid    = "AllowECRReadAccess"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::582526512915:root"]
+    }
+    principals {
+      type        = "Service"
+      identifiers = ["batch.amazonaws.com"]
+    }
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      # "ecr:CompleteLayerUpload",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      # "ecr:InitiateLayerUpload",
+      "ecr:ListImages",
+      # "ecr:PutImage",
+      # "ecr:UploadLayerPart"
+    ]
+  }
+}
+
+resource "aws_ecr_repository_policy" "data-account-readible" {
+  repository = aws_ecr_repository.etl-alphafold.name
+  policy     = data.aws_iam_policy_document.data-account-policy.json
+}
---
+++ terraform-ecr/terragrunt.hcl
@@ -0,0 +1,27 @@
+remote_state {
+  backend = "s3"
+  generate = {
+    path      = "backend.tf"
+    if_exists = "overwrite_terragrunt"
+  }
+  config = {
+    bucket  = "shiru-shared-ecr-terraform"
+    key     = "etl-alphafold/terraform.tfstate"
+    region  = "us-west-2"
+    encrypt = true
+  }
+}
+
+iam_role = "arn:aws:iam::301451991843:role/Shared-Account-Admin"
+
+generate "provider" {
+  path      = "provider.tf"
+  if_exists = "overwrite_terragrunt"
+  contents  = <<EOF
+provider "aws" {
+  version = "~> 3.22.0"
+  region  = "us-west-2"
+  allowed_account_ids = ["301451991843"]
+}
+EOF
+}
